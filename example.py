1739aa27b1a24d3f92053ee70fd88c5ca44f7a7f
Victor-BlackPC <victoraguirre.f@gmail.com>
2020-10-29 17:24:36 +0800

    20201029 example with asyncio, getting prfile camera

--- example.py
+++ example.py
@@ -1,9 +1,11 @@
-
 from onvif import ONVIFCamera
+import asyncio
 
-
-mycam = ONVIFCamera('172.16.18.110', 80, 'admin', '123456', '/etc/onvif/wsdl/')
-await mycam.update_xaddrs()
-
-resp = await mycam.devicemgmt.GetHostname()
-print('My camera`s hostname: ' + str(resp.Name))
+async def camera_():
+    mycam = ONVIFCamera('172.16.18.130', 80, 'admin', '123456')
+    await mycam.update_xaddrs()
+    media_service = mycam.create_media_service()
+    profiles = await media_service.GetProfiles()
+    print(profiles)
+    return await mycam.close()
+asyncio.run(camera_())
