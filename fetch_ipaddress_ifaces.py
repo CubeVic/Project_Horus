173403c19bb53d3affa27926a192c03f393c760a
Victor Andres Aguirre Fernandez <vic@Victors-MacBook-Pro.local>
2020-10-31 19:42:26 +0800

    20201101 update: adding play ground to research an understand libraries that can be use in the project, adding script about network interfaces

---
+++ fetch_ipaddress_ifaces.py
@@ -0,0 +1,30 @@
+""" Small script to fetch the ipaddress of the network interface
+In this case focus in the Ethernet and the internet (need to clarify that concept in the future)
+main goal is get a subnet of a scope to later look for devices withing that scope
+Example: Scope = 192.168  a device with 192.168.0.100 will be within that scope"""
+from netifaces import interfaces, ifaddresses, AF_INET
+
+# AF_INET - Address Family - Normal internet address
+
+#Todo:
+"""set logging system for this script"""
+print(f'\n Interfaces: {interfaces()} \n')
+
+def extract_scope_(interface):
+	""" it willl filter the Ethernet/internet interface
+	and return just the IP address """
+	if AF_INET in ifaddresses(interface):
+		ip_address = ifaddresses(interface)[AF_INET][0]['addr']
+		return ip_address
+
+
+scope = None 
+
+# extrat the IP "scope" of the IP range of the Ethernet interface
+if not scope : ips = list(map(extract_scope_,interfaces())) 
+
+# Extract the first two numbers of the address
+scope = ['.'.join(ip.split('.')[:2]) for ip in ips if ip]
+
+print(scope) #the scope,
+# ['127.0', '192.168'] 
