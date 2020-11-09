f1d7863f3d773f80ab9571510c1ef443c96a3b59
Victor-BlackPC <victoraguirre.f@gmail.com>
2020-11-09 16:27:38 +0800

    20201109 Update:  adding .gitignore

--- example.py
+++ example.py
@@ -2,12 +2,12 @@ from onvif import ONVIFCamera
 import asyncio
 
 async def camera_():
-    mycam = ONVIFCamera('172.16.18.151', 80, 'admin', 'Ww80503736')
+    mycam = ONVIFCamera('172.16.18.107', 80, 'admin', '123456')
     await mycam.update_xaddrs()
     media_service = mycam.create_media_service()
     profiles = await media_service.GetProfiles()
     print(f'profile {profiles[0].Name} :\n {profiles[0]}')
-    with open('A416_profile.txt','w') as file:
+    with open('A45_profile.txt','w') as file:
         file.write(str(profiles))
     # token = profiles[0].token
     # print(token)
--- fetch_webservices_wsdiscovery.py
+++ fetch_webservices_wsdiscovery.py
@@ -23,19 +23,33 @@ cloudUserName = base + 'CloudUserName/'
 acc_scope = base + 'acc/'
 
 scope1 = Scope(base) # to be use later to filter just those with ONVIF services
+scope2 = Scope(profile_scope)
+
+def fetch_devices(services):
+	for service in services:
+	#filter those devices that dont have ONVIF service
+	# print(f'\nAddress:\n{service.getXAddrs()[0]}\n')
+		ipaddress = re.search('(\d+|\.)+', str(service.getXAddrs()[0])).group(0)
+		print(f'\nIP Address: {ipaddress}')
+		for scope in service.getScopes():
+			#Scope methods getMatchBy, getQuotedValue, getValue
+			print(scope.getValue())
+	print(f'\nnumber of devices detected: {len(services)}')
 
 wsd = WSDiscovery()
 wsd.start()
-devices_services = wsd.searchServices(scopes=[scope1])
+# devices_services = wsd.searchServices(scopes=[scope1])
+devices_services = wsd.searchServices()
 
+fetch_devices(devices_services)
 
-for service in devices_services:
-	#filter those devices that dont have ONVIF service
-	# print(f'\nAddress:\n{service.getXAddrs()[0]}\n')
-	ipaddress = re.search('(\d+|\.)+', str(service.getXAddrs()[0])).group(0)
-	print(f'\nIP Address: {ipaddress}')
-	for scope in service.getScopes():
-		#Scope methods getMatchBy, getQuotedValue, getValue
-		print(scope.getValue())
-
+# for service in devices_services:
+# 	#filter those devices that dont have ONVIF service
+# 	# print(f'\nAddress:\n{service.getXAddrs()[0]}\n')
+# 	ipaddress = re.search('(\d+|\.)+', str(service.getXAddrs()[0])).group(0)
+# 	print(f'\nIP Address: {ipaddress}')
+# 	for scope in service.getScopes():
+# 		#Scope methods getMatchBy, getQuotedValue, getValue
+# 		print(scope.getValue())
+# print(f'\nnumber of devices detected: {len(devices_services)}')
 wsd.stop()
